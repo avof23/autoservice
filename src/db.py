593253a3ca79176bf846d4b62c80c99de9c9e74f
Anton Vlashchenkov <avof@ukr.net>
2023-09-12 00:35:14 +0300

    privat data connection to database

--- src/db.py
+++ src/db.py
@@ -8,10 +8,14 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
 load_dotenv()
+db_host = os.getenv('DB_HOSTNAME')
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
 db_passwd = os.getenv('DB_PASSWD')
 
+
 engine = create_engine(
-    f'postgresql+psycopg2://user_autoservice:{db_passwd}@localhost/autoservice'
+    f'postgresql+psycopg2://{db_user}:{db_passwd}@{db_host}/{db_name}'
 )
 Base = declarative_base()
 
